5d36de0232f717c2dc437e9364ef1af20a210303
Mano Vikram <manovik18@gmail.com>
2026-03-10 02:14:20 +0530

    Added serif font and updated the global css

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,4 +1,12 @@
 import "./globals.css";
+import { DM_Serif_Display } from "next/font/google";
+
+const dmSerifDisplay = DM_Serif_Display({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  variable: "--font-dm-serif-display",
+})
 
 export const metadata = {
   title: "Resume Analyzer",
@@ -8,8 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+      <body className={`${dmSerifDisplay.variable} antialiased`}>
+        <main className="px-6 md:px-12">
+          {children}
+        </main>
       </body>
     </html>
   );
