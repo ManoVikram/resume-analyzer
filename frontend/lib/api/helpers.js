ca2b4365f586906706dd7bb7ec9ad08e149d98db
Mano Vikram <manovik18@gmail.com>
2026-03-15 19:10:30 +0530

    Updated the code get the session token and pass it to the backend API call

--- frontend/lib/api/helpers.js
+++ frontend/lib/api/helpers.js
@@ -1,8 +1,14 @@
 "use server"
 
+import { createClient } from "../supabase/server"
+
 const API_BASE_URL = `${process.env.HTTP_HOST}:${process.env.HTTP_PORT}`
 
 export async function analyzeResume(file, jobDescription) {
+    // Step 1 - Create supabase server client
+    const supabase = await createClient()
+    const { data: { session } } = await supabase.auth.getSession()
+
     // Step 1 - Create FormData and append file and job description
     const formData = new FormData()
     formData.append("resume_file", file)
@@ -11,6 +17,9 @@ export async function analyzeResume(file, jobDescription) {
     // Step 2 - Send POST request to backend API
     const response = await fetch(`${API_BASE_URL}/api/v1/analyze`, {
         method: "POST",
+        headers: {
+            "Authorization": `Bearer ${session.access_token}`
+        },
         body: formData
     })
 
